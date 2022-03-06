6dd7bb60648576deb0097482ecb28a14ca89cbb8
merantauw <zholu.vlad2010@yandex.ru>
2022-03-07 04:30:47 +0500

    finished projects

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
   
+  //lazy-loading
+  $('#loading').on('click', function () {
+    $('#boxes .box:hidden').slice(0, 2).slideDown()
+      if(($('#boxes .box:hidden')).length == 0){
+      $('#loading').fadeOut('slow')
+    }
+  });
+
+
 })
